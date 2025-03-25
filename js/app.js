8f840f887abf731cb431ca7ef0b19786a7adb772
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-25 14:04:54 -0300

    feat: exibe no log a quantidade total de jogos alugados no momento

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-event.preventDefault();
+let contagemAlugados = 1;
+
+console.log(contagemAlugados);
 
 function alterarStatus(id) {
+    event.preventDefault();
+
     let jogo = document.getElementById(`game-${id}`);
     let jogoImagem = jogo.querySelector('.dashboard__item__img');
     let jogoBotao = jogo.querySelector('.dashboard__item__button');
@@ -13,17 +17,22 @@ function alterarStatus(id) {
             alert(`Não pudemos registrar a devolução do jogo. Por favor tente novamente`);
             return;
         }
-
+        contagemAlugados--;
         jogoBotao.innerHTML = 'Alugar';
         jogoBotao.classList.toggle('dashboard__item__button--return');
         jogoImagem.classList.toggle('dashboard__item__img--rented');
 
         alert(`${jogoNome} foi devolvido com sucesso. Aproveite para alugar outros!`);
+        console.log('Alugado menos 1');
+        
     } else { //aluga o jogo
+        contagemAlugados++;
         jogoBotao.innerHTML = 'Devolver';
         jogoBotao.classList.toggle('dashboard__item__button--return');
         jogoImagem.classList.toggle('dashboard__item__img--rented');
         
         alert(`${jogoNome} foi alugado com sucesso. Divirta-se`);
     }
+
+    console.log(contagemAlugados);
 }
