ef833ff6a18be831ba40f10f0e17c880aa19a086
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-25 13:17:28 -0300

    perf: melhoria de identação

--- js/app.js
+++ js/app.js
@@ -5,18 +5,24 @@ function alterarStatus(id) {
     let jogoImagem = jogo.querySelector('.dashboard__item__img');
     let jogoBotao = jogo.querySelector('.dashboard__item__button');
     let jogoNome = jogo.querySelector('.dashboard__item__name');
-
+    
     if (jogoBotao.innerHTML == 'Devolver') { //devolve o jogo
         jogoBotao.innerHTML = 'Alugar';
         jogoBotao.classList.toggle('dashboard__item__button--return');
         jogoImagem.classList.toggle('dashboard__item__img--rented');
 
-        alert(`${jogoNome.innerHTML} foi devolvido com sucesso. Aproveite para alugar outros!`)
+        alert(`${jogoNome.innerHTML} foi devolvido com sucesso. Aproveite para alugar outros!`);
     } else { //aluga o jogo
         jogoBotao.innerHTML = 'Devolver';
         jogoBotao.classList.toggle('dashboard__item__button--return');
         jogoImagem.classList.toggle('dashboard__item__img--rented');
         
-        alert(`${jogoNome.innerHTML} foi alugado com sucesso. Divirta-se`)
+        alert(`${jogoNome.innerHTML} foi alugado com sucesso. Divirta-se`);
+    }
+}
+
+function alertarDevolver() {
+    if (jogoBotao.innerHTML == 'Devolver') {
+        alert(`Você tem certeza que deseja devolver ${jogoNome.innerHTML}`);
     }
 }
