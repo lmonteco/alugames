752efbc569d190d48c82f39db6a73529d6a0c9f1
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-25 13:35:13 -0300

    feat: adiciona confirmacao ao devolver um jogo

--- js/app.js
+++ js/app.js
@@ -4,25 +4,26 @@ function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let jogoImagem = jogo.querySelector('.dashboard__item__img');
     let jogoBotao = jogo.querySelector('.dashboard__item__button');
-    let jogoNome = jogo.querySelector('.dashboard__item__name');
+    let jogoNome = jogo.querySelector('.dashboard__item__name').innerHTML;
     
     if (jogoBotao.innerHTML == 'Devolver') { //devolve o jogo
+        let confirmacao = prompt(`Para confirmar a devolução, por favor digite: ${jogoNome}`);
+
+        if (confirmacao != jogoNome) {
+            alert(`Não pudemos registrar a devolução do jogo. Por favor tente novamente`);
+            return;
+        }
+
         jogoBotao.innerHTML = 'Alugar';
         jogoBotao.classList.toggle('dashboard__item__button--return');
         jogoImagem.classList.toggle('dashboard__item__img--rented');
 
-        alert(`${jogoNome.innerHTML} foi devolvido com sucesso. Aproveite para alugar outros!`);
+        alert(`${jogoNome} foi devolvido com sucesso. Aproveite para alugar outros!`);
     } else { //aluga o jogo
         jogoBotao.innerHTML = 'Devolver';
         jogoBotao.classList.toggle('dashboard__item__button--return');
         jogoImagem.classList.toggle('dashboard__item__img--rented');
         
-        alert(`${jogoNome.innerHTML} foi alugado com sucesso. Divirta-se`);
-    }
-}
-
-function alertarDevolver() {
-    if (jogoBotao.innerHTML == 'Devolver') {
-        alert(`Você tem certeza que deseja devolver ${jogoNome.innerHTML}`);
+        alert(`${jogoNome} foi alugado com sucesso. Divirta-se`);
     }
 }
